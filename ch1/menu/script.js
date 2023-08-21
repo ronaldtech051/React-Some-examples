a073192a170a62b10cfda1fa4a386989fc3cbebe
ITSuperStar51 <rudigerwinfried831@gmail.com>
2023-08-21 22:28:10 +0200

    Updated whole project :zap:

---
+++ ch1/menu/script.js
@@ -0,0 +1,44 @@
+var Menu = React.createClass({
+  render: function(){
+    var menus = ['Home',
+      'About',
+      'Services',
+      'Portfolio',
+      'Contact us']
+    return React.createElement('div',
+      null,
+      menus.map(function(v,i){
+        return React.createElement('div',
+          {key: i},
+          React.createElement(Link, {label: v})
+        )
+      })
+    )
+}})
+
+var Link = React.createClass({
+  render: function () {
+    var url='/'
+      + this.props.label
+        .toLowerCase()
+        .trim()
+        .replace(' ', '-')
+    return React.createElement('div',
+      null,
+      React.createElement(
+        'a',
+        {href: url},
+        this.props.label
+      ),
+      React.createElement('br')
+      )
+  }
+})
+
+ReactDOM.render(
+  React.createElement(
+    Menu,
+    null
+  ),
+  document.getElementById('menu')
+)
